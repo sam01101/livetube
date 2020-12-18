0559b475bafaaef89dcbf41562f06e7867f548f3
sam01101 <lau.sam745033858@gmail.com>
2020-12-19 02:06:46 +0800

    添加一下不让报错

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='livetube',
+   version='1.0',
+   description='A module for youtube livestream',
+   author='Sam',
+   author_email='lau.sam745033858@gmail.com',
+   packages=['livetube'],
+   install_requires=['aiohttp']
+)
